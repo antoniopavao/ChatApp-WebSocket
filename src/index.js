46c6ebdad39d382793c290e21ee02ef21e749538
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-09-23 13:46:36 -0300

    Creating chat.js to serve client connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const publicDirectoryPath = path.join(__dirname, "../public");
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 app.use(express.json());
+
+io.on("connection", () => {
+  console.log("New websocket connection");
+});
 
 server.listen(port, () => {
   console.log("Server is up on port: " + port);
