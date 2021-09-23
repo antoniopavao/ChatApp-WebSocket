ec960d930211194912ad877678e22d66d1ab08fe
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-09-23 09:09:30 -0300

    Adding socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
 
 const app = express();
+const server = http.createServer(app);
 const publicDirectoryPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("Server is up on port: " + port);
 });
